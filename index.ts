2532a7347069a13ffc85b7fbbeb4cc2f2075eb29
PaoloPerroneJdk <paoloperrone1984@hotmail.com>
2021-01-21 14:36:10 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,31 +1,47 @@
-import { from, Subject } from "rxjs";
+import { from, Subject, BehaviorSubject } from "rxjs";
 import { multicast } from "rxjs/operators";
 
-const subject = new Subject<number>();
+console.log("SUBJECT");
 
+const subject = new Subject<number>();
 
 subject.subscribe(val => console.log("Ossservatore A : ", val));
 subject.subscribe(val => console.log("Ossservatore B : ", val));
 
-
 subject.next(1);
 subject.next(2);
 
-
 /* altra maniera  */
 
-const observable = from ([4,5,6])
+const observable = from([4, 5, 6]);
 
-observable.subscribe(subject) /* abbiamo creato un observable che riceve un subject che va ad effettuare un next()*/
+observable.subscribe(
+  subject
+); /* abbiamo creato un observable che riceve un subject che va ad effettuare un next()*/
 
-/*      */
+/*  MULTICAST    */
 
-const source = from([8,9,10])
-const subject1 = new Subject<number>()
-const multicasted = source.pipe(multicast(subject1))
+console.log("MULTICAST");
 
+const source = from([8, 9, 10]);
+const subject1 = new Subject<number>();
+const multicasted = source.pipe(multicast(subject1));
 
 multicasted.subscribe(val => console.log("Ossservatore B : ", val));
 multicasted.subscribe(val => console.log("Ossservatore C : ", val));
 
-multicasted.connect()
+multicasted.connect();
+
+/* ALTRA MANIERA */
+console.log("altra maniera BEHAVIORSUBJECT");
+
+const subject2 = new BehaviorSubject(0);
+
+subject2.subscribe(val => console.log("Ossservatore Paolo: ", val));
+
+subject2.next(1);
+subject2.next(2);
+
+subject2.subscribe(val => console.log("Ossservatore Mario : ", val));
+
+subject2.next(3);
