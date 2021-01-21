0fb4a37071083d76af6cbf629fd9ca6655ff1d97
PaoloPerroneJdk <paoloperrone1984@hotmail.com>
2021-01-21 12:59:27 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { from, Subject } from "rxjs";
+import { multicast } from "rxjs/operators";
 
 const subject = new Subject<number>();
 
@@ -15,6 +16,16 @@ subject.next(2);
 
 const observable = from ([4,5,6])
 
-observable.subscribe(subject)
+observable.subscribe(subject) /* abbiamo creato un observable che riceve un subject che va ad effettuare un next()*/
+
+/*      */
+
+const source = from([8,9,10])
+const subject1 = new Subject<number>()
+const multicasted = source.pipe(multicast(subject1))
 
 
+multicasted.subscribe(val => console.log("Ossservatore B : ", val));
+multicasted.subscribe(val => console.log("Ossservatore C : ", val));
+
+multicasted.connect()
